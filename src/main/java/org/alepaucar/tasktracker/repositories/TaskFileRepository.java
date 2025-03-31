85275c182c332e5427b3f8ea594d3a32c8065e20
ale-paucar <nuala.ni.ale@gmail.com>
2025-03-30 22:31:37 -0500

    Add JasonMapper

--- src/main/java/org/alepaucar/tasktracker/repositories/TaskFileRepository.java
+++ src/main/java/org/alepaucar/tasktracker/repositories/TaskFileRepository.java
@@ -5,25 +5,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TaskFileRepository {
-
+    private Path dataPath;
     public TaskFileRepository() {
+        this.dataPath=Paths.get("data/tasks.json");
     }
 
     public void createFile(){
         try{
-            Path dataPath = Paths.get("data/tasks.json");
-
-            System.out.println(System.getProperty("user.dir"));
-            /*
             if(Files.exists(dataPath)){
                 System.out.println("File already exist");
             }else {
                 Path donePath = Files.createFile(dataPath);
                 System.out.println("File is created :"+donePath.toString());
-            }*/
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
 
+    }
+
+    public void editFile(){
+        try {
+            if(Files.exists(dataPath)){
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/org/alepaucar/tasktracker/utils/JsonMapperUtil.java
@@ -0,0 +1,43 @@
+package org.alepaucar.tasktracker.utils;
+
+import java.util.Map;
+
+public class JsonMapperUtil {
+
+    // 🔹 Constructor privado para evitar instanciación
+    private JsonMapperUtil() {}
+
+    //  Métodos principales para convertir JSON y Map
+    public static String mapToJson(Map<String, Object> map) {
+        // Convierte un Map en un JSON String
+        return null;
+    }
+
+    public static Map<String, Object> jsonToMap(String json) {
+        // Convierte un JSON String en un Map
+        return  null;
+    }
+
+    public static <T> String objectToJson(T obj) {
+        // Convierte un objeto en un JSON String
+        return  null;
+    }
+
+    public static <T> T jsonToObject(String json, Class<T> clazz) {
+        // Convierte un JSON String en un objeto de la clase especificada
+        return  null;
+    }
+
+    //  Métodos auxiliares
+    public static boolean isValidJson(String json) {
+        // Verifica si un string es un JSON válido4
+        return  true;
+    }
+
+    public static String prettyPrintJson(String json) {
+        // Devuelve un JSON formateado de manera legible
+        return  null;
+    }
+}
+
+
